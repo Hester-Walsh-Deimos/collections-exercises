c9cbca4eb80b3274bfcbcdc252497955bd5f2b9e
jwhester <jameshester88@gmail.com>
2019-12-05 14:53:35 -0600

    finished #3

--- src/GradesApplication.java
+++ src/GradesApplication.java
@@ -25,14 +25,31 @@ public class GradesApplication {
         students.put("vogue", nico);
         students.put("dexter", jacob);
         students.put("squirrel", andrew);
-
+        boolean choice;
+        do {
         System.out.println("Welcome!");
         System.out.println("Here are the GitHub usernames of our students:");
         for (HashMap.Entry<String, Student> student : students.entrySet()) {
             System.out.print("|" + student.getKey() + "| ");
         }
-        System.out.println("What student would you like to see more information on?");
+        System.out.println("\n What student would you like to see more information on?");
         Scanner input = new Scanner(System.in);
+        String selection = input.nextLine();
+        if (students.get(selection) != null){
+            System.out.println("Name: " + students.get(selection).getName());
+            System.out.println("Grade: " + students.get(selection).getGradeAverage());
+        } else {
+            System.out.printf("User %s does not exist.\n", selection);
+        }
+            System.out.println("Would you like to search for another student?");
+            System.out.println("Yes/No");
+            String choose = input.nextLine();
+            if (choose.equalsIgnoreCase("yes")){
+                choice = true;
+            } else {
+                choice = false;
+            }
 
+    } while (choice);
     }
 }
