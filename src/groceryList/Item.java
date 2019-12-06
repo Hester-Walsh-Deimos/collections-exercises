79fc273b4a2c5ecc5ef800a460b4819158f8b979
jwhester <jameshester88@gmail.com>
2019-12-06 10:04:05 -0600

    java grades and grocery app

--- src/groceryList/Item.java
+++ src/groceryList/Item.java
@@ -1,14 +1,30 @@
 package groceryList;
+
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+
 public class Item {
     public String name;
     public String category;
     public int quantity;
 
-    public Item(String category, String name, int quantity){
+    public Item(String category, String name, int quantity) {
         this.category = category;
         this.name = name;
         this.quantity = quantity;
     }
+
+    public static Comparator<Item> compareName = new Comparator<Item>() {
+        @Override
+        public int compare(Item o1, Item o2) {
+
+            int value1 = o1.category.compareTo(o2.category);
+            if (value1 != 0) {
+                return value1;
+            }
+            return o1.name.compareTo(o2.name);
+
+        }
+    };
 }
--- src/groceryList/ShoppingCart.java
+++ src/groceryList/ShoppingCart.java
@@ -1,6 +1,9 @@
 package groceryList;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.Collections;
+import java.io.*;
+import java.util.*;
 public class ShoppingCart {
     static ArrayList<Item> cart = new ArrayList<>();
 
@@ -12,7 +15,7 @@ public class ShoppingCart {
             System.out.println("What meat product do you want to buy?");
             input.nextLine();
             String itemName = input.nextLine();
-            System.out.printf("How many %s do you want to buy?", itemName);
+            System.out.printf("How many %s do you want to buy?\n", itemName);
             int itemQuantity = input.nextInt();
             Item meat = new Item("Meat", itemName, itemQuantity);
             cart.add(meat);
@@ -21,7 +24,7 @@ public class ShoppingCart {
             System.out.println("What dairy product do you want to buy?");
             input.nextLine();
             String itemName = input.nextLine();
-            System.out.printf("How many %s do you want to buy?", itemName);
+            System.out.printf("How many %s do you want to buy?\n", itemName);
             int itemQuantity = input.nextInt();
             Item dairy = new Item("Dairy", itemName, itemQuantity);
             cart.add(dairy);
@@ -30,7 +33,7 @@ public class ShoppingCart {
             System.out.println("What produce product do you want to buy?");
             input.nextLine();
             String itemName = input.nextLine();
-            System.out.printf("How many %s do you want to buy?", itemName);
+            System.out.printf("How many %s do you want to buy?\n", itemName);
             int itemQuantity = input.nextInt();
             Item produce = new Item("Produce", itemName, itemQuantity);
             cart.add(produce);
@@ -39,23 +42,12 @@ public class ShoppingCart {
             System.out.println("What other product do you want to buy?");
             input.nextLine();
             String itemName = input.nextLine();
-            System.out.printf("How many %s do you want to buy?", itemName);
+            System.out.printf("How many %s do you want to buy?\n", itemName);
             int itemQuantity = input.nextInt();
             Item other = new Item("Other", itemName, itemQuantity);
             cart.add(other);
         }
-        System.out.println("Do you want to add another item?");
-        input.nextLine();
-        String yesNo = input.nextLine();
-        if (yesNo.equalsIgnoreCase("yes")){
-            addItem();
-        } else {
-            System.out.println("# | Name | Category" );
-            for (Item item : cart){
-                System.out.printf("\n%d - %s - %s", item.quantity, item.name, item.category);
-            }
 
-        }
     }
 
 
@@ -67,11 +59,26 @@ public class ShoppingCart {
         while (wantsToContinue) {
             if (choice.equalsIgnoreCase("yes")) {
                 addItem();
+                System.out.println("Do you want to add another item?");
+                String yesNo = input.nextLine();
+                if (yesNo.equalsIgnoreCase("yes")){
+                    addItem();
+                } else {
+                    Collections.sort(cart, Item.compareName);
+                    System.out.println("# | Name | Category" );
+                    for (Item item : cart){
+                        System.out.printf("\n%d - %s - %s", item.quantity, item.name, item.category);
+                    }
+                    wantsToContinue = false;
+                }
             }else {
                 wantsToContinue = false;
             }
         }
 
 
+
     }
+
+
 }
